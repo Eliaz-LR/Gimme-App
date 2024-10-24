df271f43e65bc0a44e2a9da8bd3ce22a3efd2fc0
Eliaz <47760515+Eliaz-LR@users.noreply.github.com>
2024-10-24 09:28:48 +0200

    :tada:  BASE TEMPLATE

---
+++ src/main/java/com/webtp/gimme/GimmeApplication.java
@@ -0,0 +1,13 @@
+package com.webtp.gimme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GimmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webtp/gimme/GimmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webtp.gimme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
