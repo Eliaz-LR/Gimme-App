0beda2012623abc4a32ebd1b3e1a717b376f0d97
Eliaz <47760515+Eliaz-LR@users.noreply.github.com>
2024-10-24 14:53:11 +0200

    Delete offer by ID

--- src/main/java/com/webtp/gimme/controller/OfferEndpoint.java
+++ src/main/java/com/webtp/gimme/controller/OfferEndpoint.java
@@ -3,6 +3,8 @@ package com.webtp.gimme.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.webtp.gimme.model.Offer;
@@ -28,5 +30,16 @@ public class OfferEndpoint {
     public String offersPost(@RequestBody Offer offer) {
         offerService.createOffer(offer);
         return "Offre créée";
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteOffer(@RequestParam(value = "id") Long id) {
+        boolean deleted = offerService.deleteOfferByID(id);
+        if (deleted) {
+            return ResponseEntity.ok("Offer deleted successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Offer not found");
+        }
+
     }
 }
--- src/main/java/com/webtp/gimme/service/OfferService.java
+++ src/main/java/com/webtp/gimme/service/OfferService.java
@@ -3,6 +3,8 @@ package com.webtp.gimme.service;
 import com.webtp.gimme.model.Offer;
 import com.webtp.gimme.repository.OfferRepository;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,5 +24,15 @@ public class OfferService {
 
     public void createOffer(Offer offer) {
         offerRepository.save(offer);
+    }
+
+    public boolean deleteOfferByID(Long id) {
+        Optional<Offer> offer = offerRepository.findById(id);
+        if (offer.isPresent()) {
+            offerRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
